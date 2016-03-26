12da7e33e0a82d56d43720fe2f6b42be84c62931
yukimoritani <yuuki.moritani@gmail.com>
2016-03-26 10:48:36 +0900

    Modified scripts

--- pol_calc.py
+++ pol_calc.py
@@ -6,6 +6,8 @@ import math
 import numpy
 import re
 
+ERROR=-999.
+
 def PolCalc(inname,posx,posy,expt):
     # zero magnitude: sirphot.py
     zeroj=21.
@@ -44,22 +46,38 @@ def CalcCount(inname,posx,poxy,zmag,expt):
     for l in line:
         ll=l.split()
         # x,y,flux,mag,magerr 
+        flag=0
         if math.fabs(float(ll[0])-posx)<5 and math.fabs(float(ll[1])-posy)<5 :
             I = pow(10., (zmag-float(ll[3]))/2.5) * expt
             Ierr = float(ll[4]) * I / 1.0857
+            #print inname
+            flag=1
+            break
+    #print I,Ierr
+    if flag==0 :
+            I=ERROR
+            Ierr=ERROR
     return I,Ierr
 
 def CalcStokes(Iins):
 # Ins: I00 Ie00 I22 Ie22 I45 Ie45 I67 Ie67
-    sI = sum(Iins[0::2])/2
-    tmp=[pow(x,2.) for x in Iins[1::2]]
-    #print tmp
-    sIerr = math.sqrt(sum(tmp)) /2
-    #sIerr = math.sqrt(sum(pow(Iins[1::2],2.))) /2
-    sQ = Iins[4]-Iins[0]
-    sQerr = math.sqrt(pow(Iins[5],2.)+pow(Iins[1],2.))
-    sU = Iins[6]-Iins[2]
-    sUerr = math.sqrt(pow(Iins[7],2.)+pow(Iins[3],2.))
+    if ERROR in Iins :
+        sI=ERROR
+        sIerr=ERROR
+        sQ=ERROR
+        sQerr=ERROR
+        sU=ERROR
+        sUerr=ERROR
+    else:
+        sI = sum(Iins[0::2])/2
+        tmp=[pow(x,2.) for x in Iins[1::2]]
+        #print tmp
+        sIerr = math.sqrt(sum(tmp)) /2
+        #sIerr = math.sqrt(sum(pow(Iins[1::2],2.))) /2
+        sQ = Iins[4]-Iins[0]
+        sQerr = math.sqrt(pow(Iins[5],2.)+pow(Iins[1],2.))
+        sU = Iins[6]-Iins[2]
+        sUerr = math.sqrt(pow(Iins[7],2.)+pow(Iins[3],2.))
     print "%lf %lf %lf %lf %lf %lf" % (sI,sIerr,sQ,sQerr,sU,sUerr),
     return  sI,sIerr,sQ,sQerr,sU,sUerr
 
@@ -76,8 +94,12 @@ if __name__ == '__main__':
     Sins.extend(PolCalc(sys.argv[1],posx,posy,expt))
     #Sins.extend(PolCalc(inname.rstrip("\n"),posx,posy,expt))
     # sI,sIerr,sQ,sQerr,sU,sUerr
-    PI=math.sqrt(pow(Sins[2],2.)+pow(Sins[4],2.))
-    PIerr=math.sqrt(pow(Sins[2],2.)*pow(Sins[3],2.)+pow(Sins[4],2.)*pow(Sins[5],2.))/PI
-    P=PI/Sins[0]
-    Perr=math.sqrt(pow(PIerr/Sins[0],2.)+pow(P*Sins[1]/Sins[0],2.))
+    if ERROR in Sins : 
+        P=ERROR
+        Perr=ERROR
+    else :
+        PI=math.sqrt(pow(Sins[2],2.)+pow(Sins[4],2.))
+        PIerr=math.sqrt(pow(Sins[2],2.)*pow(Sins[3],2.)+pow(Sins[4],2.)*pow(Sins[5],2.))/PI
+        P=PI/Sins[0]
+        Perr=math.sqrt(pow(PIerr/Sins[0],2.)+pow(P*Sins[1]/Sins[0],2.))
     print "%lf %lf" % (P,Perr)
